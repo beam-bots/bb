135259fe9ad8ee1a413759b5f25d4020ce758dc2
James Harton <james@harton.nz>
2025-11-27 14:07:57 +1300

    🔥 initial commit 🔥

---
+++ lib/kinetix.ex
@@ -0,0 +1,18 @@
+defmodule Kinetix do
+  @moduledoc """
+  Documentation for `Kinetix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kinetix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kinetix_test.exs
@@ -0,0 +1,8 @@
+defmodule KinetixTest do
+  use ExUnit.Case
+  doctest Kinetix
+
+  test "greets the world" do
+    assert Kinetix.hello() == :world
+  end
+end
