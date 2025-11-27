2076d9224f297cdda59f8c4d8481d86904a3d2fd
James Harton <james@harton.nz>
2025-11-27 14:46:23 +1300

    chore: release version v0.1.0

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Kinetix
 
-**TODO: Add description**
+Kinetix is a framework for building resilient robotics projects in Elixir.
+
+## Status
+
+No work has started yet.  TODO's include:
+
+- `Kinetix.Message` protocol for serialisation and deserialisation of messages.
+- `Kinetix.PubSub` cluster-aware pubsub mechanism for kinetic messages.
+- `Kinetix.Topology` extensible DSL for describing robot toplogies.
+
+Additional packages will likely be needed:
+
+- `kinetix_sitl` simulation of Kinetix systems inside Gazebo, etc.
+- `kinetix_rc_servo` subscriber which drives PWM-based RC-style servos.
+- `kinetix_mavlink` mavlink bridge.
+- `kinetix_csrf` crossfire bridge.
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `kinetix` to your list of dependencies in `mix.exs`:
+Kinetix is not yet available in hex, but you can try it by adding it to your project as a Git dependency:
 
 ```elixir
 def deps do
@@ -14,8 +28,4 @@ def deps do
   ]
 end
 ```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/kinetix>.
 
--- mix.exs
+++ mix.exs
@@ -1,13 +1,33 @@
 defmodule Kinetix.MixProject do
   use Mix.Project
 
+  @moduledoc """
+  The framework for resilient robotics.
+  """
+
+  @version "0.1.0"
+
   def project do
     [
       app: :kinetix,
-      version: "0.1.0",
+      version: @version,
+      description: @moduledoc,
+      package: package(),
+      docs: docs(),
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["James Harton <james@harton.nz>"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "Source" => "https://harton.dev/kinetix/kinetix",
+        "Sponsor" => "https://github.com/sponsors/jimsynz"
+      }
     ]
   end
 
@@ -18,12 +38,23 @@ defmodule Kinetix.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:igniter, "~> 0.6", only: [:dev, :test]}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test]},
+      {:dialyxir, "~> 1.4", only: [:dev, :test]},
+      {:ex_check, "~> 0.16", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
+      {:git_ops, "~> 2.9", only: [:dev, :test]},
+      {:igniter, "~> 0.6", only: [:dev, :test]},
+      {:mix_audit, "~> 2.1", only: [:dev, :test]}
     ]
   end
 end
